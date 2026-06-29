f594e67e675ed11ceb78a2c8c484ca42a3631fa6
eddynoet <eddytorrico.sis24@tecba.edu.bo>
2026-06-29 11:05:39 -0400

    themes modification

--- lib/core/theme/map_styles.dart
+++ lib/core/theme/map_styles.dart
@@ -2,7 +2,6 @@ abstract final class MapStyles {
   static const String dark = '''
 [
   {"elementType":"geometry","stylers":[{"color":"#212121"}]},
-  {"elementType":"labels.icon","stylers":[{"visibility":"off"}]},
   {"elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},
   {"elementType":"labels.text.stroke","stylers":[{"color":"#212121"}]},
   {"featureType":"administrative","elementType":"geometry","stylers":[{"color":"#757575"}]},

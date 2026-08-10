b726a3067b19e8298e238a8f1d18b4c563f054b9
David Jesus Villarroel <davidvillarroel.sis24@tecba.edu.bo>
2026-08-09 21:22:53 -0400

    conflictos de wili resuelto

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -46,16 +46,10 @@ import 'package:mi_ruta/features/user/data/repositories/location_repository_impl
 import 'package:mi_ruta/features/user/domain/usecases/get_current_location_usecase.dart';
 import 'package:mi_ruta/features/user/domain/usecases/reverse_geocode_usecase.dart';
 import 'package:mi_ruta/features/user/presentation/bloc/mi_ruta_bloc.dart';
-<<<<<<< HEAD
-import 'package:mi_ruta/features/routes/domain/services/gtfs_schedule_service.dart';
-import 'package:mi_ruta/features/driver/data/datasources/driver_datasource.dart';
-import 'package:mi_ruta/features/driver/domain/services/driver_service.dart';
-=======
 import 'package:mi_ruta/features/user/domain/services/user_preferences_service.dart';
 import 'package:mi_ruta/features/user/presentation/bloc/user_bloc.dart';
 import 'package:mi_ruta/features/user/presentation/bloc/user_preferences_bloc.dart';
 
->>>>>>> origin/dev-samuel-chambi
 
 final getIt = GetIt.instance;
 
